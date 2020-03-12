5c7e2d20a8ca3347a2376c182203bd8b876f87c3
Vincent Zhou <fireflysuccess@gmail.com>
2020-03-13 07:36:17 +0800

    Added now thoughts
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- src/grader/grader.h
+++ src/grader/grader.h
@@ -1,27 +1,27 @@
 /* define a structure, with the function pointer,
-which is just like a class (**cough**, mantained 
-by ourselved) 
+which is just like a class (**cough**, mantained
+by ourselved)
 
-for netfilter, how does it do? 
+for netfilter, how does it do?
 
 could do it tomorrow!! */
 
-/* define the funtion pointer here, 
+/* define the funtion pointer here,
 then called in different position */
 
 /* in the netfilter,
  the hook pointer will call the NF_HOOK ,
 -> nf_hook_slow -> call in the related position
 -> call the funtions defined in the nf_hooks_ops,
-which can be registered (injected to) 
+which can be registered (injected to)
 -> then the function will be called in the right
 time.
 */
 /** still the relation is not very clear, need fur-
 ther study**/
 
-/* thus, for a grader, just hook it to the right 
-place, 
+/* thus, for a grader, just hook it to the right
+place,
 but the grader could be for function, for file and
 for folder(module). Thus, just different hook point
 at different stages, i.e., when entering folder, en-
@@ -39,14 +39,18 @@ trigger when every time enter a file, enter a folder...
 thus: related structure: folder, file, function should
 be defined,
 
-trigged ones will call the right function to update the 
+trigged ones will call the right function to update the
 grade.
 
-Then where do we store the grade?? just put it in the 
+Then where do we store the grade?? just put it in the
 tree node (thus, define GRADER_FUN_IN.. things to diff
 different kinds of grades)
 
--- does the target and match resued in the netfilter, 
-maybe that's why it is registered in a different 
+-- does the target and match resued in the netfilter,
+maybe that's why it is registered in a different
 double-linked link.
+*/
 
+/* thus, need to analyse the netfilter first, too better
+ * do this job
+ */
